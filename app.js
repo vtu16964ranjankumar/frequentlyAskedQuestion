4dd7e7b642d535932148f1eb2789e5cc7779f651
Ranjan Kumar <vtu16964@veltech.edu.in>
2024-01-31 21:17:15 +0530

    Updated

--- app.js
+++ app.js
@@ -17,14 +17,3 @@ questions.forEach(function (question) {
     question.classList.toggle("show-text");
   });
 });
-
-// traversing the dom
-// const btns = document.querySelectorAll(".question-btn");
-
-// btns.forEach(function (btn) {
-//   btn.addEventListener("click", function (e) {
-//     const question = e.currentTarget.parentElement.parentElement;
-
-//     question.classList.toggle("show-text");
-//   });
-// });
